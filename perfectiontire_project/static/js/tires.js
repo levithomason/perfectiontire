f3669fd5c171a479cecb76c9fdc6ea926bb284e3
ckcollab <eric@ckcollab.com>
2013-10-06 02:19:15 -0700

    added tire imgs and data!

---
+++ perfectiontire_project/static/js/tires.js
@@ -0,0 +1,54 @@
+var PRODUCT_DATA = [
+    {
+        tires: [
+            {
+                brand: 'Cooper',
+                products: ['Zeon RS3', 'Zeon RS3-A', 'Zeon RS3-S', 'Zeon XST-A']
+            },
+            {
+                brand: 'Nexen',
+                products: ['N5000', 'N7000']
+            },
+            {
+                brand: 'Hercules',
+                products: ['Raptis TR1', 'Raptis VR1', 'Raptis WR1']
+            },
+            {
+                brand: 'Fuzion',
+                products: ['ZRI']
+            }
+        ],
+        wheels: [
+            {
+                brand: 'AKUZA'
+            },
+            {
+                brand: 'BALLISTIC'
+            },
+            {
+                brand: 'BOSS'
+            },
+            {
+                brand: 'Incubus'
+            },
+            {
+                brand: 'KMC'
+            },
+            {
+                brand: 'LIQUIDMETAL'
+            },
+            {
+                brand: 'Motegi Racing'
+            },
+            {
+                brand: 'MSR'
+            },
+            {
+                brand: 'REV'
+            },
+            {
+                brand: 'Sendel'
+            }
+        ]
+    }
+];
