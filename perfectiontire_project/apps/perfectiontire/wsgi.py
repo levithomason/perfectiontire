96b33ec58350b3edb05032e6365e78505fc97723
ckcollab <eric@ckcollab.com>
2013-10-06 00:03:51 -0700

    fix client

--- perfectiontire_project/apps/perfectiontire/wsgi.py
+++ perfectiontire_project/apps/perfectiontire/wsgi.py
@@ -8,10 +8,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perfectiontire_project.settings
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 from dj_static import Cling
+from raven import Client
 from raven.middleware import Sentry
+
 
 # Static file management
 application = Cling(get_wsgi_application())
 
-client = settings.SENTRY_DSN
+client = Client(settings.SENTRY_DSN)
 application = Sentry(application, client=client)
