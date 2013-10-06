8bd6791d0895860e7dd8372770eb12b35d53ab2a
ckcollab <eric@ckcollab.com>
2013-10-06 00:02:43 -0700

    removed pycs

--- perfectiontire_project/apps/perfectiontire/wsgi.py
+++ perfectiontire_project/apps/perfectiontire/wsgi.py
@@ -1,11 +1,17 @@
 import os
 
+
 # We have to setup django settings up here
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perfectiontire_project.settings.production")
 
 # Cling requires some stuff in settings, so make sure we import it AFTER settings!
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 from dj_static import Cling
+from raven.middleware import Sentry
 
 # Static file management
 application = Cling(get_wsgi_application())
+
+client = settings.SENTRY_DSN
+application = Sentry(application, client=client)
