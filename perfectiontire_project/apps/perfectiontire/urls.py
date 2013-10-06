1d99f4235d139323ebda4926e532c5fb29bf96fd
ckcollab <eric@ckcollab.com>
2013-10-06 00:35:43 -0700

    home page added

--- perfectiontire_project/apps/perfectiontire/urls.py
+++ perfectiontire_project/apps/perfectiontire/urls.py
@@ -1,12 +1,12 @@
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'perfectiontire.views.home', name='home'),
+    url(r'^$', 'perfectiontire.views.home', name='home'),
     # url(r'^perfectiontire/', include('perfectiontire.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
