0b378a2a5398a313c57dd41b0a55d98612de2862
ckcollab <eric@ckcollab.com>
2013-10-06 18:54:01 -0700

    got it to work!

--- perfectiontire_project/static/js/perfectiontire/productPage.js
+++ perfectiontire_project/static/js/perfectiontire/productPage.js
@@ -1,19 +1,36 @@
+angular.module('app', [])
+    // To properly use isotope, we need to wait for ng-repeat to finish before trying to render!
+    .directive('onFinishRender', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                });
+            }
+        }
+    }
+});
+
 function ProductPageController($scope) {
     var container = $('#isotope_container');
 
+    $scope.filterType = '.tire';
     $scope.productData = PRODUCT_DATA;
 
-    console.log(PRODUCT_DATA);
-
-    var init = function() {
-
-        container.isotope({
-            itemSelector: '.isotope-item',
-            layoutMode: 'fitRows'
-        });
-
+    $scope.filterIsotope = function(selector) {
+        $scope.filterType = selector;
+        container.isotope({ filter: selector });
         container.isotope('reLayout');
     };
 
-    init();
+    $scope.initIsotope = function() {
+        container.isotope();
+    };
+
+    // Wait for ng-repeat to finish, then add data!
+    $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+        $scope.initIsotope();
+    });
 }
