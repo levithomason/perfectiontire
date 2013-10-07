7c36faa452289208cc1fd75fe662ebd0bc344889
Levi Thomason <me@levithomason.com>
2013-10-06 22:56:30 -0700

    adds all tire images, splits wheel_brand data out, adds category filter

--- perfectiontire_project/static/js/perfectiontire/tires.js
+++ perfectiontire_project/static/js/perfectiontire/tires.js
@@ -5,61 +5,71 @@ var PRODUCT_DATA = [
         brand: 'Cooper',
         category: 'Ultra High Performance',
         name: 'Zeon RS3'
+        image: 'Zeon RS3.png'
     },
     {
         type: 'tire',
         brand: 'Cooper',
         category: 'Ultra High Performance',
-        name: 'Zeon RS3-A'
+        name: 'Zeon RS3-A',
+        image: 'Zeon RS3-A.png'
     },
     {
         type: 'tire',
         brand: 'Cooper',
         category: 'Ultra High Performance',
-        name: 'Zeon RS3-S'
+        name: 'Zeon RS3-S',
+        image: 'Zeon RS3-S.png'
     },
     {
         type: 'tire',
         brand: 'Cooper',
         category: 'Ultra High Performance',
-        name: 'Zeon XST-A'
+        name: 'Zeon XST-A',
+        image: 'ZEON XST-A.png'
     },
 
     {
         type: 'tire',
         brand: 'Nexen',
         category: 'Ultra High Performance',
-        name: 'N5000'
+        name: 'N5000',
+        image: 'n5000.png'
     },
     {
         type: 'tire',
         brand: 'Nexen',
         category: 'Ultra High Performance',
-        name: 'N7000'
+        name: 'N7000',
+        image: 'n7000.png'
     },
     {
         type: 'tire',
         brand: 'Hercules',
         category: 'Ultra High Performance',
         name: 'Raptis TR1'
+        image: 'hercules-raptis-tr1'
     },
     {
         type: 'tire',
         brand: 'Hercules',
         category: 'Ultra High Performance',
         name: 'Raptis VR1'
+        image: ''
     },
     {
         type: 'tire',
         brand: 'Hercules',
         category: 'Ultra High Performance',
         name: 'Raptis WR1'
+        image: 'hercules-raptis-vr1.png'
     },
     {
         type: 'tire',
         brand: 'Fuzion',
         category: 'Ultra High Performance',
         name: 'ZRI'
+        image: 'Fuzion-Zri.png'
     },
 
     // Tires - High Performance
@@ -68,81 +78,35 @@ var PRODUCT_DATA = [
         brand: 'Cooper',
         category: 'High Performance',
         name: 'Zeon ZPT'
+        image: 'ZEON ZPT.png'
     },
     {
         type: 'tire',
         brand: 'Cooper',
         category: 'High Performance',
         name: 'Lifeliner GLS'
+        image: 'LIFELINER-GLS.png'
     },
     {
         type: 'tire',
         brand: 'Nexen',
         category: 'High Performance',
         name: 'CP641'
+        image: ''
     },
     {
         type: 'tire',
         brand: 'Fuzion',
         category: 'High Performance',
         name: 'HRI'
+        image: 'fuzion-hri.png'
     },
     {
         type: 'tire',
         brand: 'Starfire',
         category: 'High Performance',
         name: 'RS-C 2.0'
+        image: 'rs-c2.png'
     },
 
-    // Wheels
-    {
-        type: 'wheel',
-        brand: 'AKUZA',
-        image: 'img/wheel_brands/akuza.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'BALLISTIC',
-        image: 'img/wheel_brands/ba;;ostoc.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'BOSS',
-        image: 'img/wheel_brands/boss.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'Incubus',
-        image: 'img/wheel_brands/incubus.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'KMC',
-        image: 'img/wheel_brands/kmc.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'LIQUIDMETAL',
-        image: 'img/wheel_brands/liquid-metal.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'Motegi Racing',
-        image: 'img/wheel_brands/motegi.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'MSR',
-        image: 'img/wheel_brands/MSR.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'REV',
-        image: 'img/wheel_brands/rev.png'
-    },
-    {
-        type: 'wheel',
-        brand: 'Sendel',
-        image: 'img/wheel_brands/sendel.png'
-    }
 ];
---
+++ perfectiontire_project/static/js/perfectiontire/wheel_brands.js
@@ -0,0 +1,52 @@
+var WHEEL_BRANDS = [
+    {
+        type: 'wheel_brand',
+        brand: 'AKUZA',
+        image: 'akuza.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'BALLISTIC',
+        image: 'ba;;ostoc.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'BOSS',
+        image: 'boss.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'Incubus',
+        image: 'incubus.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'KMC',
+        image: 'kmc.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'LIQUIDMETAL',
+        image: 'liquid-metal.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'Motegi Racing',
+        image: 'motegi.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'MSR',
+        image: 'MSR.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'REV',
+        image: 'rev.png'
+    },
+    {
+        type: 'wheel_brand',
+        brand: 'Sendel',
+        image: 'sendel.png'
+    }
+];
