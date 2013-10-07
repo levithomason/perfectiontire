8f681b31fc5de8b3d1385ca960f8969bd25c042d
ckcollab <eric@ckcollab.com>
2013-10-06 17:33:26 -0700

    working on products page, added shortcut icon

--- perfectiontire_project/apps/perfectiontire/urls.py
+++ perfectiontire_project/apps/perfectiontire/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
 from .views.home import home
+from .views.products import products
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', home, name='home'),
+    url(r'^products/$', products, name='products'),
     # url(r'^perfectiontire/', include('perfectiontire.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
